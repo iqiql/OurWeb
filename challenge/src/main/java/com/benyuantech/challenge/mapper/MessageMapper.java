62361f081cfbb35a83fb95209f2902a377fddcff
iqiql <94536241+iqiql@users.noreply.github.com>
2021-12-10 11:05:03 +0800

    添加了标记，实际上使用的方法是insert，这个方法可以更快的生成sql语句

--- challenge/src/main/java/com/benyuantech/challenge/mapper/MessageMapper.java
+++ challenge/src/main/java/com/benyuantech/challenge/mapper/MessageMapper.java
@@ -5,6 +5,6 @@ import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface MessageMapper {
-    int insert(Message message);// 添加留言
+    int insert(Message message);// 添加留言，这是实际使用的方法
     int insertSelective(Message message);// 添加留言
 }
